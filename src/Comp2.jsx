a3bf4edff628cf48673d3551ac81214cc7a2b332
BMold <bmold8@gmial.com>
2025-12-04 15:25:12 -0400

    finalfinals

--- src/Comp2.jsx
+++ src/Comp2.jsx
@@ -1,4 +1,4 @@
-import Card from "./Card"
+import Tarjeta from "./Tarjeta.jsx";
 // 1. IMPORTA LAS IMÁGENES (Asegúrate de que estén en src/assets)
 // Si tus imagenes se llaman diferente, ajusta el nombre aquí
 import img1 from './assets/1.jpg' 
@@ -8,8 +8,8 @@ function Comp2() {
   return (
     <>
     <div>
-        <Card ruta={img1} titulo="Tarjeta de Prueba" contenido="contenido de prueba" id_tarjeta="1" />
-        <Card ruta={img3} titulo="Tarjeta de Final" contenido="xd" id_tarjeta="6" />
+        <Tarjeta ruta={img1} titulo="Tarjeta de Prueba" contenido="contenido de prueba" id_tarjeta="1" />
+        <Tarjeta ruta={img3} titulo="Tarjeta de Final" contenido="xd" id_tarjeta="6" />
     </div>
     </>
   )
--- src/Tarjeta.jsx
+++ src/Tarjeta.jsx
@@ -1,4 +1,3 @@
-import './App.css'
 function Tarjeta(props) {
 
   return (
